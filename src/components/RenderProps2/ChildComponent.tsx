ed68a24a7e7f08430fee583fe5b74e6378802f07
KortexDev <cacortesl01@gmail.com>
2024-12-02 11:37:52 -0500

    render prop 2 ejercicio

--- src/components/RenderProps2/ChildComponent.tsx
+++ src/components/RenderProps2/ChildComponent.tsx
@@ -1,13 +1,17 @@
 
+import React from "react";
+
 type ChildComponentProps = {
-  
+  data: string[],
 }
 
-const ChildComponent = () => {
+const ChildComponent: React.FC<ChildComponentProps> = ({data}) => {
   return (
     <>
+      {data.map((item) => <li key={item}>{item}</li>)}
     </>
   );
 };
 
 export default ChildComponent;
+
--- src/components/RenderProps2/RenderProps2.tsx
+++ src/components/RenderProps2/RenderProps2.tsx
@@ -1,13 +1,10 @@
+import ChildComponent from "./ChildComponent";
 import ParentComponent from "./ParentComponent";
 
 export const RenderProps2 = () => {
   return (
     <div>
-      <ParentComponent render={(data)=> data.map((item, index) => {
-        return <li key={index}>{item}</li>
-      })}>
-        
-      </ParentComponent>
+      <ParentComponent render={(data: string[])=> <ChildComponent data={data}/>}/>
     </div>
   );
 };
