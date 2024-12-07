a4e2bb69f75c6f1dde5b6f0cdbac2e4978c75ed2
KortexDev <cacortesl01@gmail.com>
2024-12-07 11:54:13 -0500

    componente controlado e incontrolado, ejercicio

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,22 @@
+import React, { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
+
+  const onEventChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const textValue: string = e.target.value;
+    setValue(textValue);
+  }
+
   return (
     <div>
+      <input 
+        type="text" 
+        value={value} 
+        onChange={onEventChange} 
+        placeholder="Ingrese cupón, ejemplo: XD323LOL"
+      />
+      <p>Tu cupón de descuento es: <b>{value}</b></p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,35 @@
+import React, { useRef } from "react";
+
 export const UncontrolledInput = () => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const input2Ref = useRef<HTMLInputElement>(null);
+
+  const handleClickBtnSubmit: React.MouseEventHandler<HTMLButtonElement> = () => {
+    console.log(inputRef.current)
+    const inputValue = inputRef.current?.value;
+    const inputValue2 = input2Ref.current?.value;
+    
+    if(inputValue && inputValue2){
+      alert(`Has agregado el producto "${inputValue}" al carrito, cantidad: ${inputValue2}`)
+    }else{
+      alert("Debes agregar un producto y la cantidad")
+    }
+  }
+
   return (
     <div>
+      <input 
+        ref={inputRef}
+        type="text" 
+        placeholder="Nombre del producto, ejemplo: Banana"
+      />
+      <input 
+        ref={input2Ref}
+        type="number" 
+        placeholder="Ingresa la cantidad"
+      />
+      <button onClick={handleClickBtnSubmit}>Ingresar al carrito</button>
     </div>
   );
 };
